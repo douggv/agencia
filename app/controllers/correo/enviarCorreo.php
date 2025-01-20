7cd2d0df4a21641435de3534c4b5a94402304652
Douglas <douglasgv0502@gmail.com>
2025-01-21 00:00:56 +0100

    cambosphpm,ailer

--- app/controllers/correo/enviarCorreo.php
+++ app/controllers/correo/enviarCorreo.php
@@ -3,31 +3,35 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 
 if (isset($_SESSION["tituloEmail"])) {
-    $titulo = $_SESSION["tituloEmail"];
-    $mensaje = $_SESSION["mensajeEmail"];
-    $email = $_SESSION["emailEnviar"];
-    unset($_SESSION["tituloEmail"]);
-    unset($_SESSION["mensajeEmail"]);
-    unset($_SESSION["emailEnviar"]);
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
-    require 'phpmailer/src/SMTP.php';
+    try {
+        $titulo = $_SESSION["tituloEmail"];
+        $mensaje = $_SESSION["mensajeEmail"];
+        $email = $_SESSION["emailEnviar"];
+        unset($_SESSION["tituloEmail"]);
+        unset($_SESSION["mensajeEmail"]);
+        unset($_SESSION["emailEnviar"]);
+        require 'phpmailer/src/Exception.php';
+        require 'phpmailer/src/PHPMailer.php';
+        require 'phpmailer/src/SMTP.php';
 
-    $mail = new PHPMailer(true);
-    $mail ->IsSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = "natalyurribarri23@gmail.com";
-    $mail->Password = "vdzj mbuo ynxl iemc";
-    $mail->SMTPSecure = "ssl";
-    $mail->Port = 465;
+        $mail = new PHPMailer(true);
+        $mail ->IsSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = "natalyurribarri23@gmail.com";
+        $mail->Password = "vdzj mbuo ynxl iemc";
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-    $mail->setFrom("natalyurribarri23@gmail.com");
-    $mail->addAddress($email); // correo al que enviaremos
-    $mail->isHTML(true);
-    $mail->Subject = $titulo;
-    $mail->Body = '<h1>'.$mensaje.'</h1>';
-    $mail ->send();
+        $mail->setFrom("natalyurribarri23@gmail.com");
+        $mail->addAddress($email); // correo al que enviaremos
+        $mail->isHTML(true);
+        $mail->Subject = $titulo;
+        $mail->Body = '<h1>'.$mensaje.'</h1>';
+        $mail ->send();
 
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
